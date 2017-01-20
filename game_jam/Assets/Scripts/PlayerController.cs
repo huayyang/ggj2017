8391c2c95cd6f7d8e6bc9ee79005f20a9e76c843
Huayu Yang <huayu@storm8.com>
2017-01-20 07:21:36 -0800

    123

---
+++ game_jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Components
+	private Rigidbody2D mRigidbody;
+	private WaveController mWaveController;
+	private GameObject mWaveCollider;
+
+	// Movements
+	public float mMoveSpeed = 5f;
+	public float mJumpSpeed = 8f;
+	public float mClimbSpeed = 3f;
+
+	// Ability
+	private WaveController.WaveType currentWaveType;
+	private int currentWaveAmount;
+	
+	// Use this for initialization
+	void Start () {
+		mRigidbody = this.GetComponent<Rigidbody2D>();
+		mWaveController = GameObject.FindGameObjectWithTag("waveCollider").GetComponent<WaveController>();
+		mWaveCollider = GameObject.FindGameObjectWithTag("waveCollider");
+		currentWaveType = WaveController.WaveType.North;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		handlePlayerMovement(Time.deltaTime);
+		handleCastWaves();
+	}
+
+	void handlePlayerMovement(float deltaTime) {
+		bool isWalking = false;
+		Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal") * mMoveSpeed, Input.GetAxisRaw("Vertical") * mJumpSpeed);
+		if (movement != Vector2.zero) {
+			mRigidbody.AddForce(movement);
+		}
+	}
+
+	private bool canCastWave() {
+		if (currentWaveAmount <= 0) {
+			return false;
+		}
+
+		if (mWaveController.canCastWave()) {
+			return false;
+		}
+
+		return true;
+	}
+	public void handleCastWaves() {
+		if (Input.GetKeyDown(KeyCode.A) && canCastWave()) {
+			mWaveController.castWave(currentWaveType);
+		}
+	}
+	bool canWaveHitObject(Vector3 playerPosition, Vector3 targetPosition) {
+		RaycastHit2D hit = Physics2D.Raycast(playerPosition, targetPosition - playerPosition);
+		if (hit != null && hit.collider.CompareTag("waveBlocker")) {
+			return false;
+		}
+		return true;
+	}
+	public void onWaveHitObject(Collider2D collider) {
+		if (!canWaveHitObject(mWaveCollider.transform.position, collider.transform.position)) {
+			return;
+		}
+		Debug.Log("Player OnWaveHit");
+	}
+
+	public void increaseWaveAmount(int amount) {
+		currentWaveAmount += amount;
+	}
+}
---
+++ game_jam/Assets/Scripts/WaveController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+
+	private PlayerController mPlayerController;
+	private CircleCollider2D mCircleCollider;
+	private float waveCastCoolDown = 3.0f;
+	private float waveCastTimer;
+	public float waveSpeed = 0.5f;
+	public enum WaveType {North, East, South, West};
+	public WaveType mWaveType;
+	public float waveMaxRadius = 10.0f;
+
+	private float waveInitialRadius = 0.5f;
+	// Use this for initialization
+	void Start () {
+		mPlayerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+		mCircleCollider = this.GetComponent<CircleCollider2D>();
+		mCircleCollider.radius = waveInitialRadius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (waveCastTimer > 0) {
+			waveCastTimer -= Time.deltaTime;
+		}
+	}
+	public bool canCastWave() {
+		return waveCastTimer <= 0;
+	}
+	public void castWave(WaveType waveType) {
+		mWaveType = waveType;
+		waveCastTimer = waveCastCoolDown;
+		StartCoroutine(waveStart());
+	}
+
+	IEnumerator waveStart() {
+		while (mCircleCollider.radius < waveMaxRadius) {
+			mCircleCollider.radius += waveSpeed * Time.deltaTime;
+			//Debug.Log("wave radius: " + mCircleCollider.radius);
+			yield return null;
+		}
+
+		mCircleCollider.radius = waveInitialRadius;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (!collider.CompareTag("hitObject")) {
+			return;
+		}
+
+		Debug.Log("Wave OnTriggerEnter");
+		mPlayerController.onWaveHitObject(collider);
+	}
+
+	
+}
