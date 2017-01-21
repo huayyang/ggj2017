c9486f74fcbd70a383d8ca93ca1b2d33fc63468f
Huayu Yang <huayu@storm8.com>
2017-01-20 18:50:54 -0800

    disappear trap

---
+++ game_jam/Assets/Scripts/DisappearTrapController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearTrapController : MonoBehaviour {
+	public float disappearTime = 3.0f;
+	private bool startDisappearCountDown = false;
+	// Use this for initialization
+	void Start () {
+		startDisappearCountDown = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startDisappearCountDown) {
+			disappearTime -= Time.deltaTime;
+		}
+
+		if (disappearTime <= 0) {
+			Destroy(gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collider) {
+		if (!collider.gameObject.CompareTag("Player")) {
+			return;
+		}
+
+		startDisappearCountDown = true;
+	}
+}
